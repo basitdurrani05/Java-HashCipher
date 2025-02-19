ce431806b69bd40e3215000035d090af1cf4877f
basitdurrani <basitdurrani05786@gmail.com>
2025-02-19 21:18:18 +0530

    Inheritance

---
+++ eleventh.java
@@ -0,0 +1,216 @@
+// Inheritance 
+
+//  Types
+
+// 1. Single Inheritance
+// 2. Multiple Inheritance (Access Special Method which name is Interface)
+// 3. Multilevel Inheritance
+// 4. Hierarchical Inheritance
+// 5. Hybird Inheritance
+
+
+// 1. Single Inheritance
+
+
+// class A{
+
+//     static void show(){
+
+//         System.out.println("This is Class A....");
+//     }
+
+//     static void show2(){
+
+//         System.out.println("Arun");
+//     }
+
+//     static void show3(){
+
+//         System.out.println("Amit");
+//     }
+
+//     static void show4(){
+
+//         System.out.println("Asheesh");
+//     }
+// }
+
+
+
+// Multi-Level Inheritance
+
+// class A{
+
+//     void KOKILA(){
+
+//         System.out.println("I AM KOKILA MODI");
+//     }
+
+
+
+
+// }
+
+
+// class B extends A{
+
+//     void ahem(){
+
+//         System.out.println("Ahem");
+//     }
+// }
+
+// class c extends B{
+
+//     void newbornbaby(){
+
+//         System.out.println("Ahem ka Baccha");
+//     }
+// }
+
+
+
+
+// Hierarchical Inheritance
+
+
+
+// class Arun{
+
+//     void show(){
+
+//         System.out.println("Arun");
+//     }
+//     void show2(){
+
+//         System.out.println("Arun Wife");
+//     }
+
+   
+
+
+// }
+
+// class A extends Arun{
+
+//     void show3(){
+
+//         System.out.println("New Born Baby Boy.");
+       
+//     }
+// }
+
+// class C extends A{
+
+//     void show4(){
+
+//         System.out.println("New Born Baby Girl.");
+//     }
+// }
+
+
+
+// Hybrid Inheritance
+
+
+// class A{
+
+
+//     void show1(){
+
+//         System.out.println("Grand Father");
+//     }
+// }
+
+
+// class B extends A{
+
+//     void show2(){
+
+//         System.out.println("Father (Son)");
+//     }
+// }
+
+
+// class c extends B{
+
+//     void show3(){
+
+//         System.out.println("Son (Father)");
+//     }
+// }
+
+
+
+
+
+
+
+
+public class eleventh{
+
+
+
+
+    public static void main(String[] args) {
+
+        // Single Inheritance
+
+        // show();
+        // show2();
+        // show3();
+        // show4();
+
+
+        // Multi-Level Inheritance
+
+
+
+        // eleventh sc = new eleventh();
+
+        // sc.KOKILA();
+        // sc.ahem();
+        // sc.newbornbaby();
+
+
+        // Hierarchical Inheritance
+
+        
+
+        // eleventh sc = new eleventh();
+        
+        // sc.show();
+        // sc.show2();
+        // sc.show3();
+        // sc.show4();
+
+
+
+        // Hybrid Inheritance
+
+
+        // eleventh sc = new eleventh();
+
+        // sc.show1();
+        // sc.show2();
+        // sc.show3();
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+        
+    }
+    
+}
